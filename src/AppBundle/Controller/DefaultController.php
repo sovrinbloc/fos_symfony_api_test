76d68088a4ce35e4410dc376fe8d5160e3c93cc8
Joseph Alai <josephalai@gmail.com>
2018-03-16 18:43:45 +0000

    Stylizing form inside twig

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+
+
 class DefaultController extends Controller
 {
     /**
@@ -18,4 +23,38 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ));
     }
+
+    /**
+     * @Route("/form", name="form")
+     */
+    public function newAction(Request $request)
+    {
+        // createFormBuilder is a shortcut to get the "form factory"
+        // and then call "createBuilder()" on it
+
+        $defaults = array(
+            'dueDate' => new \DateTime('tomorrow'),
+            'todaysDate' => new \DateTime('today'),
+        );
+
+        $gratitudeDefaults = array(
+            'gratefulFor'=> "I am grateful for being alive today.",
+        );
+
+        $form = $this->createFormBuilder($defaults)
+            ->add('task', new TextType)
+            ->add('dueDate', new DateType)
+            ->add('todaysDate', new DateType)
+            ->getForm();
+
+        $gratitudeForm = $this->createFormBuilder($gratitudeDefaults)
+            ->add('gratefulFor', new TextType)
+            ->getForm();
+
+        return $this->render('default/new.html.twig', array(
+            'form' => $form->createView(),
+            'form2' => $gratitudeForm->createView()
+        ));
+    }
+
 }
