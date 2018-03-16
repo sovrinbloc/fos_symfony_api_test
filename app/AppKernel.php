df816ada41e7c68fe7d0fcb71a548f6080ad643e
Joseph Alai <josephalai@gmail.com>
2018-03-16 21:51:07 +0000

    Installs to just before API Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,11 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
 
 
         );
